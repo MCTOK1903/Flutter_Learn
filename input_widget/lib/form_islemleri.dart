4075cb8c19551ef851d26d56221ad50470efaf10
MCT <celaltok2@gmail.com>
2020-05-17 15:27:36 +0300

    created textFormWidget

--- input_widget/lib/form_islemleri.dart
+++ input_widget/lib/form_islemleri.dart
@@ -6,19 +6,90 @@ class FormIslemleri extends StatefulWidget {
 }
 
 class _FormIslemleriState extends State<FormIslemleri> {
+  String textArea = "";
+  int maxLines = 1;
+  FocusNode fnode;
+  TextEditingController textEditingController1;
+
+  @override
+  void initState() {
+    super.initState();
+    fnode = FocusNode();
+    textEditingController1 = TextEditingController(text: "selam");
+    fnode.addListener(() {
+      setState(() {
+        if (fnode.hasFocus) {
+          maxLines = 3;
+        } else { 
+          maxLines = 1;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    fnode.dispose();
+    textEditingController1.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Selam"),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //FocusScope.of(context).requestFocus(fnode);
+          print(textEditingController1.text.toString());
+        },
+        child: Icon(Icons.add_box),
+      ),
       body: ListView(
         children: <Widget>[
-          TextField(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "as ",
+              ),
+              maxLines: maxLines,
+              autofocus: false,
+              controller: textEditingController1,
+              focusNode: fnode,
+              textInputAction: TextInputAction.next,
+              onChanged: (x) {
+                textArea = x;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "as ",
+              ),
+              maxLines: maxLines,
+              autofocus: false,
+              textInputAction: TextInputAction.next,
+              maxLength: 5,
+              onChanged: (x) {
+                textArea = x;
+                print(x);
+              },
+            ),
+          ),
           Container(
+            margin: EdgeInsets.all(8),
             color: Colors.green,
             width: double.infinity,
             height: 200,
+            child: Align(
+              alignment: Alignment.center,
+              child: Text("$textArea"),
+            ),
           ),
         ],
       ),
